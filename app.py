d512991e0d99e490f28c4312a8c20356c5f204a1
DIALLO Mamadou Cherif <mamadcherifdiallo001@gmail.com>
2025-09-28 22:22:48 +0200

    Ajout du pipeline

--- app.py
+++ app.py
@@ -2,24 +2,37 @@ from flask import Flask, render_template, request
 import pickle
 import pandas as pd
 
+'''
+Catboost on a déjà entrainé le modèle et on a sauvegardé le modèle dans un fichier pickle
+Nous on veut construire une application web qui va charger ce modèle et faire des prédictions avec flask
+'''
 
-app = Flask(__name__)
-model = pickle.load(open("catboost_model-2.pkl", "rb"))
+app = Flask(__name__) # création de l'application flask
+model = pickle.load(open("catboost_model-2.pkl", "rb")) # chargement du modèle
+
 
 
 def model_pred(features):
+    '''
+    Fonction pour faire des prédictions avec le modèle chargé
+    '''''
     test_data = pd.DataFrame([features])
     prediction = model.predict(test_data)
     return int(prediction[0])
 
-
+# definir notre application avec des routes @ ça s'appelle des décorateurs
 @app.route("/", methods=["GET"])
 def Home():
     return render_template("index.html")
 
 
 @app.route("/predict", methods=["POST"])
+# une seule route qui est predict
 def predict():
+    '''Fonction pour faire des prédictions avec le modèle chargé
+    permet de demander à l'utilisateur d'entrer les valeurs des caractéristiques
+    et afficher le résultat de la prédiction sur la page web
+    '''
     if request.method == "POST":
         Age = int(request.form["Age"])
         RestingBP = int(request.form["RestingBP"])
@@ -48,3 +61,5 @@ def predict():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+    # ici on va demarrer le serveur flask
+    # host  = "                                 
